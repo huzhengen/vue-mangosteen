3a9249e96403a8c6efce6290877962c2f9ac91e1
huzhengen <huzhengen@163.com>
2022-04-26 15:07:57 +0800

    WelcomeLayout 组件

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,27 +1,32 @@
 import { defineComponent } from 'vue'
-import s from './First.module.scss'
+import s from './WelcomeLayout.module.scss'
 import pig from '../../assets/icons/pig.svg'
 import { RouterLink } from 'vue-router'
+import { WelcomeLayout } from './WelcomeLayout'
 export const First = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img src={pig} alt="pig" />
-          <h2>
-            会赚钱
-            <br />
-            还要会省钱
-          </h2>
-        </div>
-        <div class={s.actions}>
-          <RouterLink class={s.fake} to="/start">
-            跳过
-          </RouterLink>
-          <RouterLink to="/welcome/2">下一页</RouterLink>
-          <RouterLink to="/start">跳过</RouterLink>
-        </div>
-      </div>
+      <WelcomeLayout>
+        {{
+          icon: () => <img src={pig} alt="pig" />,
+          title: () => (
+            <h2>
+              会赚钱
+              <br />
+              还要会省钱
+            </h2>
+          ),
+          actions: () => (
+            <>
+              <RouterLink class={s.fake} to="/start">
+                跳过
+              </RouterLink>
+              <RouterLink to="/welcome/2">下一页</RouterLink>
+              <RouterLink to="/start">跳过</RouterLink>
+            </>
+          ),
+        }}
+      </WelcomeLayout>
     )
   },
 })
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,10 +1,32 @@
 import { defineComponent } from 'vue'
+import s from './WelcomeLayout.module.scss'
+import chart from '../../assets/icons/chart.svg'
+import { RouterLink } from 'vue-router'
+import { WelcomeLayout } from './WelcomeLayout'
 export const Third = defineComponent({
-  setup(props, context) {
+  setup: (props, context) => {
     return () => (
-      <div>
-        <h1>Third</h1>
-      </div>
+      <WelcomeLayout>
+        {{
+          icon: () => <img src={chart} alt="chart" />,
+          title: () => (
+            <h2>
+              数据可视化
+              <br />
+              收支一目了然
+            </h2>
+          ),
+          actions: () => (
+            <>
+              <RouterLink class={s.fake} to="/start">
+                跳过
+              </RouterLink>
+              <RouterLink to="/welcome/4">下一页</RouterLink>
+              <RouterLink to="/start">跳过</RouterLink>
+            </>
+          ),
+        }}
+      </WelcomeLayout>
     )
   },
 })
