46fdbf55198182dd7dd9ad490bebe09d4e1a3fc2
hukai <huzhengen@163.com>
2022-05-23 18:43:35 +0800

    Charts 组件

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,5 +1,6 @@
 import { defineComponent, PropType, ref } from 'vue'
 import s from './Charts.module.scss'
+import { FormItem } from '../../shared/Form'
 
 export const Charts = defineComponent({
   props: {
@@ -14,6 +15,18 @@ export const Charts = defineComponent({
   },
   setup: (props, context) => {
     const category = ref('expenses')
-    return () => <div class={s.wrapper}></div>
+    return () => (
+      <div class={s.wrapper}>
+        <FormItem
+          label="类型"
+          type="select"
+          options={[
+            { value: 'expenses', text: '支出' },
+            { value: 'income', text: '收入' },
+          ]}
+          v-model={category.value}
+        />
+      </div>
+    )
   },
 })
--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -29,13 +29,14 @@ export const FormItem = defineComponent({
     },
     type: {
       type: String as PropType<
-        'text' | 'emojiSelect' | 'date' | 'validationCode'
+        'text' | 'emojiSelect' | 'date' | 'validationCode' | 'select'
       >,
     },
     error: {
       type: String,
     },
     placeholder: String,
+    options: Array as PropType<Array<{ value: string; text: string }>>,
   },
   emits: ['update:modelValue'],
   setup: (props, context) => {
@@ -78,6 +79,20 @@ export const FormItem = defineComponent({
                 发送验证码
               </Button>
             </>
+          )
+        case 'select':
+          return (
+            <select
+              class={[s.formItem, s.select]}
+              value={props.modelValue}
+              onChange={(e: any) => {
+                context.emit('update:modelValue', e.target.value)
+              }}
+            >
+              {props.options?.map((option) => (
+                <option value={option.value}>{option.text}</option>
+              ))}
+            </select>
           )
         case 'date':
           return (
