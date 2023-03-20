e6b5d5aca870f34f3f659abdafef2a6dc1337eb3
huzhengen <huzhengen@163.com>
2023-03-20 17:09:52 +0800

    Add useTags.tsx

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -4,6 +4,7 @@ import { Button } from '../../shared/Button';
 import { http } from '../../shared/Http';
 import { Icon } from '../../shared/Icon';
 import { Tabs, Tab } from '../../shared/Tabs';
+import { useTags } from '../../shared/useTags';
 import { InputPad } from './InputPad';
 import s from './ItemCreate.module.scss';
 export const ItemCreate = defineComponent({
@@ -13,33 +14,18 @@ export const ItemCreate = defineComponent({
     }
   },
   setup: (props, context) => {
-    const refExpensesTags = ref<Tag[]>([])
-    const refIncomeTags = ref<Tag[]>([])
     const refKind = ref('支出')
-    const refPage = ref(0)
-    const refHasMore = ref(false)
 
-    const fetchTags = async () => {
-      const response = await http.get<Resources<Tag>>('/tags',
-        { kind: 'expenses', page: refPage.value + 1, _mock: 'tagIndex' }
-      )
-      const { resources, pager } = response.data
-      refExpensesTags.value.push(...resources)
-      refHasMore.value = (pager.page - 1) * pager.per_page + resources.length < pager.count
-      refPage.value += 1
+    const { tags: expensesTags, hasMore, fetchTags } = useTags((page) => http.get<Resources<Tag>>('/tags',
+      { kind: 'expenses', page: page + 1, _mock: 'tagIndex' }
+    ))
 
-    }
-    const onLoadMore = () => fetchTags()
-
-    // expenses
-    onMounted(() => fetchTags())
-
-    // income
-    onMounted(async () => {
-      const response = await http.get<{ resources: Tag[] }>('/tags',
-        { kind: 'income', _mock: 'tagIndex' }
-      )
-      refIncomeTags.value = response.data.resources
+    const { tags: incomeTags, hasMore: hasMore2, fetchTags: fetchTags2 } = useTags((page) => {
+      return http.get<Resources<Tag>>('/tags', {
+        kind: 'income',
+        page: page + 1,
+        _mock: 'tagIndex'
+      })
     })
 
     return () => (
@@ -59,7 +45,7 @@ export const ItemCreate = defineComponent({
                       新增
                     </div>
                   </div>
-                  {refExpensesTags.value.map(tag =>
+                  {expensesTags.value.map(tag =>
                     <div class={[s.tag, s.selected]}>
                       <div class={s.sign}>
                         {tag.sign}
@@ -71,30 +57,37 @@ export const ItemCreate = defineComponent({
                   )}
                 </div>
                 <div class={s.more}>
-                  {refHasMore.value
-                    ? <Button onClick={onLoadMore}>加载更多标签</Button>
+                  {hasMore.value
+                    ? <Button onClick={fetchTags}>加载更多标签</Button>
                     : <span>没有更多标签了</span>}
                 </div>
               </Tab>
-              <Tab name="收入" class={s.tags_wrapper}>
-                <div class={s.tag}>
-                  <div class={s.sign}>
-                    <Icon name="add" class={s.createTag} />
-                  </div>
-                  <div class={s.name}>
-                    新增
-                  </div>
-                </div>
-                {refIncomeTags.value.map(tag =>
-                  <div class={[s.tag, s.selected]}>
+              <Tab name="收入">
+                <div class={s.tags_wrapper}>
+                  <div class={s.tag}>
                     <div class={s.sign}>
-                      {tag.sign}
+                      <Icon name="add" class={s.createTag} />
                     </div>
                     <div class={s.name}>
-                      {tag.name}
+                      新增
                     </div>
                   </div>
-                )}
+                  {incomeTags.value.map(tag =>
+                    <div class={[s.tag, s.selected]}>
+                      <div class={s.sign}>
+                        {tag.sign}
+                      </div>
+                      <div class={s.name}>
+                        {tag.name}
+                      </div>
+                    </div>
+                  )}
+                </div>
+                <div class={s.more}>
+                  {hasMore2.value
+                    ? <Button onClick={fetchTags2}>加载更多标签</Button>
+                    : <span>没有更多标签了</span>}
+                </div>
               </Tab>
             </Tabs>
             <div class={s.inputPad_wrapper}>
