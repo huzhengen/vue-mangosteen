c11a1852686a55e0abe01ea8a14473c6802a328a
huzhengen <huzhengen@163.com>
2023-03-21 15:13:18 +0800

    Update InputPad.tsx

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,55 +1,33 @@
-import { DatetimePicker, Popup } from 'vant'
-import { defineComponent, PropType, ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { Icon } from '../../shared/Icon'
-import { Time } from '../../shared/time'
-import s from './InputPad.module.scss'
-
+import { defineComponent, PropType, ref } from 'vue';
+import { Icon } from '../../shared/Icon';
+import { Time } from '../../shared/time';
+import s from './InputPad.module.scss';
+import { DatetimePicker, NumberKeyboard, Popup } from 'vant';
 export const InputPad = defineComponent({
   props: {
-    modelValue: {
-      type: String as PropType<string>,
-    },
-    onSubmit: {
-      type: Function as PropType<() => void>,
-    },
-  },
-  emits: ['update:modelValue'],
-  setup: (props, context) => {
-    const router = useRouter()
-    const now = new Date()
-    const refDate = ref(now)
-    const refDatePickerVisible = ref(false)
-    const showDatePicker = () => (refDatePickerVisible.value = true)
-    const hideDatePicker = () => (refDatePickerVisible.value = false)
-    const setDate = (date: Date) => {
-      refDate.value = date
-      hideDatePicker()
+    name: {
+      type: String as PropType<string>
     }
-    const refAmount = ref('0')
+  },
+  setup: (props, context) => {
+    const now = new Date()
+    const refDate = ref<Date>(now)
     const appendText = (n: number | string) => {
       const nString = n.toString()
       const dotIndex = refAmount.value.indexOf('.')
       if (refAmount.value.length >= 13) {
-        // 超过13位就不再追加
         return
       }
       if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
-        // 已经有小数点了，小数点后最多两位
         return
       }
       if (nString === '.') {
-        // 输入的是小数点
-        if (dotIndex >= 0) {
-          // 已经有小数点了
+        if (dotIndex >= 0) { // 已经有小数点了
           return
         }
       } else if (nString === '0') {
-        // 输入的是0
-        if (dotIndex === -1) {
-          // 没有小数点
-          if (refAmount.value === '0') {
-            // 没小数点，但是有0
+        if (dotIndex === -1) { // 没有小数点
+          if (refAmount.value === '0') { // 没小数点，但是有0
             return
           }
         }
@@ -61,117 +39,45 @@ export const InputPad = defineComponent({
       refAmount.value += n.toString()
     }
     const buttons = [
-      {
-        text: '1',
-        onClick: () => {
-          appendText(1)
-        },
-      },
-      {
-        text: '2',
-        onClick: () => {
-          appendText(2)
-        },
-      },
-      {
-        text: '3',
-        onClick: () => {
-          appendText(3)
-        },
-      },
-      {
-        text: '4',
-        onClick: () => {
-          appendText(4)
-        },
-      },
-      {
-        text: '5',
-        onClick: () => {
-          appendText(5)
-        },
-      },
-      {
-        text: '6',
-        onClick: () => {
-          appendText(6)
-        },
-      },
-      {
-        text: '7',
-        onClick: () => {
-          appendText(7)
-        },
-      },
-      {
-        text: '8',
-        onClick: () => {
-          appendText(8)
-        },
-      },
-      {
-        text: '9',
-        onClick: () => {
-          appendText(9)
-        },
-      },
-      {
-        text: '.',
-        onClick: () => {
-          appendText('.')
-        },
-      },
-      {
-        text: '0',
-        onClick: () => {
-          appendText(0)
-        },
-      },
-      {
-        text: '清空',
-        onClick: () => {
-          refAmount.value = '0'
-        },
-      },
-      {
-        text: '提交',
-        onClick: () => {
-          context.emit('update:modelValue', refAmount.value)
-          props?.onSubmit?.()
-        },
-      },
+      { text: '1', onClick: () => { appendText(1) } },
+      { text: '2', onClick: () => { appendText(2) } },
+      { text: '3', onClick: () => { appendText(3) } },
+      { text: '4', onClick: () => { appendText(4) } },
+      { text: '5', onClick: () => { appendText(5) } },
+      { text: '6', onClick: () => { appendText(6) } },
+      { text: '7', onClick: () => { appendText(7) } },
+      { text: '8', onClick: () => { appendText(8) } },
+      { text: '9', onClick: () => { appendText(9) } },
+      { text: '.', onClick: () => { appendText('.') } },
+      { text: '0', onClick: () => { appendText(0) } },
+      { text: '清空', onClick: () => { refAmount.value = '0' } },
+      { text: '提交', onClick: () => { } },
     ]
-    return () => (
-      <div>
-        <div class={s.dateAndAmount}>
-          <span class={s.date}>
-            <Icon name="date" class={s.icon} />
-            <span>
-              <span onClick={showDatePicker}>
-                {new Time(refDate.value).format()}
-              </span>
-              <Popup
-                position="bottom"
-                v-model:show={refDatePickerVisible.value}
-              >
-                <DatetimePicker
-                  value={refDate.value}
-                  type="date"
-                  title="选择年月日"
-                  onConfirm={setDate}
-                  onCancel={hideDatePicker}
-                />
-              </Popup>
-            </span>
+    const refDatePickerVisible = ref(false)
+    const showDatePicker = () => refDatePickerVisible.value = true
+    const hideDatePicker = () => refDatePickerVisible.value = false
+    const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
+    const refAmount = ref('0')
+    return () => <>
+      <div class={s.dateAndAmount}>
+        <span class={s.date}>
+          <Icon name="date" class={s.icon} />
+          <span>
+            <span onClick={showDatePicker}>{new Time(refDate.value).format()}</span>
+            <Popup position='bottom' v-model:show={refDatePickerVisible.value}>
+              <DatetimePicker value={refDate.value} type="date" title="选择年月日"
+                onConfirm={setDate} onCancel={hideDatePicker}
+              />
+            </Popup>
           </span>
-          <span class={s.amount}>{refAmount.value}</span>
-        </div>
-        <div class={s.buttons}>
-          {buttons.map((button) => (
-            <button onClick={button.onClick}>{button.text}</button>
-          ))}
-        </div>
+        </span>
+        <span class={s.amount}>{refAmount.value}</span>
       </div>
-    )
-  },
+      <div class={s.buttons}>
+        {buttons.map(button =>
+          <button onClick={button.onClick}>{button.text}</button>
+        )}
+      </div>
+    </>
+  }
 })
