1e1ec69a8adb0f631e8fc4da30c6f696572306c6
huzhengen <huzhengen@163.com>
2023-05-09 17:44:05 +0800

    Deploy

---
+++ bin/deploy.sh
@@ -0,0 +1,51 @@
+# 注意修改 user 和 ip
+user=ubuntu
+ip=111.11.11.11
+
+time=$(date +'%Y%m%d-%H%M%S')
+cache_dir=tmp/deploy_cache
+current_dir=$(dirname $0)
+deploy_dir=/home/$user/deploys/$time
+frontend_dir=$cache_dir/frontend
+
+function title {
+  echo 
+  echo "###############################################################################"
+  echo "## $1"
+  echo "###############################################################################" 
+  echo 
+}
+
+mkdir -p $cache_dir
+
+title '删除远程 deploys 目录'
+ssh $user@$ip "rm -rf /home/$user/deploys/"
+title '创建远程目录'
+ssh $user@$ip "mkdir -p $deploy_dir/vendor"
+
+title '打包前端代码'
+mkdir -p $frontend_dir
+# rm -rf $frontend_dir/repo
+# git clone git@github.com:huzhengen/vue-mangosteen.git $frontend_dir/repo
+# cd $frontend_dir/repo && pnpm install && pnpm run build; cd -
+pnpm run build # 可配置，TODO
+tar -cz -f "$frontend_dir/dist.tar.gz" -C "$current_dir/../dist" .
+
+title '上传前端代码'
+scp "$frontend_dir/dist.tar.gz" $user@$ip:$deploy_dir/
+yes | rm -rf $frontend_dir
+
+title '上传 nginx 配置文件'
+scp $current_dir/../config/sk.vue.nginx.conf $user@$ip:$deploy_dir/
+scp $current_dir/../config/sk.react.nginx.conf $user@$ip:$deploy_dir/
+scp $current_dir/../config/blog.vue.nginx.conf $user@$ip:$deploy_dir/
+scp $current_dir/../config/nginx.conf $user@$ip:$deploy_dir/
+
+title '上传 setup 脚本'
+scp $current_dir/setup_remote.sh $user@$ip:$deploy_dir/
+
+title '上传版本号'
+ssh $user@$ip "echo $time > $deploy_dir/version"
+
+title '执行远程脚本'
+ssh $user@$ip "export version=$time; /bin/bash $deploy_dir/setup_remote.sh"
---
+++ bin/setup_remote.sh
@@ -0,0 +1,35 @@
+user=ubuntu
+root=/home/$user/deploys/$version
+container_name=mangosteen-prod-1
+nginx_container_name=mangosteen-nginx-1
+
+function title {
+  echo
+  echo "###############################################################################"
+  echo "## $1"
+  echo "###############################################################################"
+  echo
+}
+
+if [ "$(sudo docker ps -aq -f name=^${nginx_container_name}$)" ]; then
+  title 'frontend: docker rm'
+  sudo docker rm -f $nginx_container_name
+fi
+
+title 'frontend: docker run'
+cd /home/$user/deploys/$version
+mkdir ./distskvue
+tar xf dist.tar.gz --directory=./distskvue
+cd -
+sudo docker run -d -p 80:80 \
+           --name=$nginx_container_name \
+           -v /home/$user/deploys/$version/sk.vue.nginx.conf:/etc/nginx/conf.d/sk.vue.nginx.conf \
+           -v /home/$user/deploys/$version/sk.react.nginx.conf:/etc/nginx/conf.d/sk.react.nginx.conf \
+           -v /home/$user/deploys/$version/blog.vue.nginx.conf:/etc/nginx/conf.d/blog.vue.nginx.conf \
+           -v /home/$user/deploys/$version/nginx.conf:/etc/nginx/nginx.conf \
+           -v /home/$user/deploys/$version/distskvue:/usr/share/nginx/html/skvue \
+           -v /home/$user/deploys/$version/distskreact:/usr/share/nginx/html/skreact \
+           -v /home/rails-share-blog/public:/usr/share/nginx/html/blogvue \
+           nginx:latest
+
+title '全部执行完毕'
