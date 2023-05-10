67cb43ebbf876d4b672f318bf924c071d414402e
huzhengen <huzhengen@163.com>
2023-05-10 17:37:01 +0800

    Remove mock when running build

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,6 +7,8 @@ declare module '*.vue' {
   export default component
 }
 
+declare var DEBUG: boolean
+
 type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
 
 type Tag = {
--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,5 +1,4 @@
 import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
-import { mockItemCreate, mockItemIndex, mockItemIndexBalance, mockItemSummary, mockSession, mockTagEdit, mockTagIndex, mockTagShow } from "../mock/mock";
 import { Toast } from "vant";
 
 type GetConfig = Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>
@@ -26,38 +25,6 @@ export class Http {
   delete<R = unknown>(url: string, query?: Record<string, string>, config?: DeleteConfig) {
     return this.instance.request<R>({ ...config, url: url, params: query, method: 'delete' })
   }
-}
-
-const mock = (response: AxiosResponse) => {
-  if (true || location.hostname !== 'localhost'
-    && location.hostname !== '127.0.0.1') { return false }
-  switch (response.config?._mock) {
-    case 'tagIndex':
-      [response.status, response.data] = mockTagIndex(response.config)
-      return true
-    case 'session':
-      [response.status, response.data] = mockSession(response.config)
-      return true
-    case 'itemCreate':
-      [response.status, response.data] = mockItemCreate(response.config)
-      return true
-    case 'tagShow':
-      [response.status, response.data] = mockTagShow(response.config)
-      return true
-    case 'tagEdit':
-      [response.status, response.data] = mockTagEdit(response.config)
-      return true
-    case 'itemIndex':
-      [response.status, response.data] = mockItemIndex(response.config)
-      return true
-    case 'itemIndexBalance':
-      [response.status, response.data] = mockItemIndexBalance(response.config)
-      return true
-    case 'itemSummary':
-      [response.status, response.data] = mockItemSummary(response.config)
-      return true
-  }
-  return false
 }
 
 function isDev() {
@@ -94,21 +61,75 @@ http.instance.interceptors.response.use((response) => {
   throw error
 })
 
-http.instance.interceptors.response.use((response) => {
-  mock(response)
-  if (response.status >= 400) {
-    throw { response }
-  } else {
-    return response
-  }
-}, (error) => {
-  mock(error.response)
-  if (error.response.status >= 400) {
-    throw error
-  } else {
-    return error.response
-  }
-})
+if (DEBUG) {
+  import('../mock/mock').then(
+    ({
+      mockItemCreate,
+      mockItemIndex,
+      mockItemIndexBalance,
+      mockItemSummary,
+      mockSession,
+      mockTagEdit,
+      mockTagIndex,
+      mockTagShow
+    }) => {
+      const mock = (response: AxiosResponse) => {
+        if (
+          true ||
+          (location.hostname !== 'localhost' &&
+            location.hostname !== '127.0.0.1')
+        ) {
+          return false
+        }
+        switch (response.config?._mock) {
+          case 'tagIndex':
+            ;[response.status, response.data] = mockTagIndex(response.config)
+            return true
+          case 'session':
+            ;[response.status, response.data] = mockSession(response.config)
+            return true
+          case 'itemCreate':
+            ;[response.status, response.data] = mockItemCreate(response.config)
+            return true
+          case 'tagShow':
+            ;[response.status, response.data] = mockTagShow(response.config)
+            return true
+          case 'tagEdit':
+            ;[response.status, response.data] = mockTagEdit(response.config)
+            return true
+          case 'itemIndex':
+            ;[response.status, response.data] = mockItemIndex(response.config)
+            return true
+          case 'itemIndexBalance':
+            ;[response.status, response.data] = mockItemIndexBalance(response.config)
+            return true
+          case 'itemSummary':
+            ;[response.status, response.data] = mockItemSummary(response.config)
+            return true
+        }
+        return false
+      }
+      http.instance.interceptors.response.use(
+        (response) => {
+          mock(response)
+          if (response.status >= 400) {
+            throw { response }
+          } else {
+            return response
+          }
+        },
+        (error) => {
+          mock(error.response)
+          if (error.response.status >= 400) {
+            throw error
+          } else {
+            return error.response
+          }
+        }
+      )
+    }
+  )
+}
 
 http.instance.interceptors.response.use(
   response => response,
